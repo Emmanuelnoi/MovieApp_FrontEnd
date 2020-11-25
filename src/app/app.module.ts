33f391bc5e73cc64b10888389454d06bfb812bcb
Emmanuel Noi <Emmanuel.noi@student.fairfield.edu>
2020-11-25 14:12:11 -0500

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { ServicesComponent } from './services/services.component';
+import { PageNavigatorComponent } from './page-navigator/page-navigator.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+    ServicesComponent,
+    PageNavigatorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
